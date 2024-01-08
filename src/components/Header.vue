06824ed5e0136c8996c8da0639a07b4fcaa2571f
fojogrimmo <afanasyvekarindm@gmail.com>
2024-01-08 23:08:40 +0300

    initial commit

---
+++ src/components/Header.vue
@@ -0,0 +1,24 @@
+<template>
+  <header class="flex justify-between px-24 py-10 text-2xl">
+    <div class="flex items-center">
+      <h2 class="text-4xl font-bold">3legant<span class="text-stone-500">.</span></h2>
+    </div>
+
+    <ul class="flex items-center gap-10">
+      <li class="flex items-center gap-3 cursor-pointer hover:text-emerald-400">
+        <img src="/cart.svg" alt="Cart" class="w-8" />
+        <b><span>&#8364;</span>1205 </b>
+      </li>
+
+      <li class="flex items-center gap-3 cursor-pointer hover:text-emerald-400">
+        <img src="/heart.svg" alt="Heart" class="w-6" />
+        <span>Wishlist</span>
+      </li>
+
+      <li class="flex items-center gap-3 cursor-pointer hover:text-emerald-400">
+        <img src="/profile.svg" alt="Profile" class="w-7" />
+        <span>My Profile</span>
+      </li>
+    </ul>
+  </header>
+</template>
