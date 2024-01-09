ccfa27ebf317714260bb0afd19cb0cd38f27dac1
fojogrimmo <afanasyvekarindm@gmail.com>
2024-01-09 15:32:22 +0300

    Add component for cards rendering

---
+++ src/components/CardList.vue
@@ -0,0 +1,22 @@
+<script setup>
+import Card from './Card.vue'
+
+const onClickAdd = () => {
+  alert('Added to cart')
+}
+</script>
+
+<template>
+  <div class="grid grid-cols-4 gap-5 w-fit m-auto">
+    <Card
+      title="Nike - Tiger Woods '13"
+      imageUrl="/items/sneakers-1.png"
+      :price="239.99"
+      :onClickAdd="onClickAdd"
+    />
+    <Card />
+    <Card />
+    <Card />
+    <Card />
+  </div>
+</template>
