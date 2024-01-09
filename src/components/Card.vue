4ee3894e39444216352d3116db1ffd7180f4932a
fojogrimmo <afanasyvekarindm@gmail.com>
2024-01-09 15:31:29 +0300

    Add card component

---
+++ src/components/Card.vue
@@ -0,0 +1,46 @@
+<script setup>
+defineProps({
+  title: String,
+  imageUrl: String,
+  price: Number,
+  isFavorite: Boolean,
+  isAdded: Boolean,
+  onClickAdd: Function,
+  onClickFavorite: Function
+})
+</script>
+
+<template>
+  <div
+    class="relative w-72 bg-white border border-slate-100 rounded-b-lg cursor-pointer transition hover:-translate-y-2 hover:shadow"
+  >
+    <div class="flex justify-between">
+      <span class="absolute top-6 left-5 bg-white font-bold p-1 rounded-lg">HOT</span>
+      <img
+        :src="!isFavorite ? '/like-1.svg' : '/like-2.svg'"
+        alt="Like 1"
+        class="absolute top-6 right-5 scale-125"
+        @click="onClickFavorite"
+      />
+    </div>
+
+    <img :src="imageUrl" alt="Sneakers" />
+
+    <div class="p-2">
+      <p class="font-medium mt-1">{{ title }}</p>
+      <div class="flex justify-between">
+        <div class="flex flex-col">
+          <p>Men's Golf Shoes</p>
+          <span class="font-medium mt-4">&#8364;{{ price }}</span>
+        </div>
+
+        <img
+          @click="onClickAdd"
+          :src="!isAdded ? '/plus.svg' : 'checked.svg'"
+          alt="Plus"
+          class="scale-125 mr-4"
+        />
+      </div>
+    </div>
+  </div>
+</template>
