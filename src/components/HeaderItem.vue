2f074ecffba4c92d9a0ae6bb4f4801d02a0d805b
fojogrimmo <afanasyvekarindm@gmail.com>
2024-01-16 20:46:18 +0300

    Finalize the header item (text on picture) styles

---
+++ src/components/HeaderItem.vue
@@ -0,0 +1,22 @@
+<template>
+  <div class="relative items-center">
+    <img class="m-auto min-w-full px-16" src="/cover.jpg" alt="Cover image" />
+    <div class="absolute top-40 left-40 p-20 w-4/12">
+      <h2
+        class="mb-5 text-7xl font-bold text-transparent bg-clip-text bg-gradient-to-r from-zinc-300 via-slate-100 to-zinc-500"
+      >
+        More than just a game. <br />
+        It's a lifestyle.
+      </h2>
+      <p class="mb-8 text-2xl text-white pr-2 font-thin">
+        Whether you're just starting out, have played your whole life, or you're a Tour pro, your
+        swing is like a fingerprint.
+      </p>
+      <button
+        class="bg-emerald-400 text-black text-xl font-medium px-14 py-4 rounded hover:bg-emerald-500 transition"
+      >
+        Shopping Now
+      </button>
+    </div>
+  </div>
+</template>
