dd6768fbb0415be0811282fc32f280870391330b
fojogrimmo <afanasyvekarindm@gmail.com>
2024-01-20 13:41:37 +0300

    Add POST, DELETE methods for server

--- src/api/server.js
+++ src/api/server.js
@@ -1,5 +1,5 @@
 import express from 'express'
-import { readFile } from 'fs/promises'
+import { readFile, writeFile } from 'fs/promises'
 import cors from 'cors'
 
 const app = express()
@@ -13,6 +13,10 @@ app.use((req, res, next) => {
   res.setHeader('Access-Control-Allow-Credentials', 'true')
   next()
 })
+
+const calculateFavoriteId = (favorites) => {
+  return favorites.map((favorite, index) => ({ ...favorite, favorite_id: index + 1 }))
+}
 
 app.get('/api/products', async (req, res) => {
   try {
@@ -35,6 +39,58 @@ app.get('/api/products', async (req, res) => {
     )
 
     res.json(filteredProducts)
+  } catch (error) {
+    console.error('Error:', error)
+  }
+})
+
+app.get('/api/favorites', async (req, res) => {
+  try {
+    const favoritesData = await readFile(new URL('./favorites.json', import.meta.url), 'utf-8')
+    const favorites = JSON.parse(favoritesData)
+    res.json(favorites)
+  } catch (error) {
+    console.log('Error:', error)
+  }
+})
+
+app.post('/api/favorites', async (req, res) => {
+  try {
+    const favorite = req.body
+    const favoritesData = await readFile(new URL('./favorites.json', import.meta.url), 'utf-8')
+    const favorites = JSON.parse(favoritesData)
+    if (favorite.favorite_id) {
+      favorites.push(favorite)
+    } else {
+      const lastFavorite = favorites[favorites.length - 1]
+      const newFavorite = {
+        favorite_id: lastFavorite ? lastFavorite.favorite_id + 1 : 1,
+        item_id: favorite.item_id
+      }
+      favorites.push(newFavorite)
+    }
+    await writeFile(new URL('./favorites.json', import.meta.url), JSON.stringify(favorites))
+    res.json(favorites)
+  } catch (error) {
+    console.error('Error:', error)
+  }
+})
+
+app.delete('/api/favorites/:favorite_id', async (req, res) => {
+  try {
+    const favoriteIdDelete = parseInt(req.params.favorite_id, 10)
+    const favoritesData = await readFile(new URL('./favorites.json', import.meta.url), 'utf-8')
+    let favorites = JSON.parse(favoritesData)
+
+    const indexToDelete = favorites.findIndex(
+      (favorite) => favorite.favorite_id === favoriteIdDelete
+    )
+    if (indexToDelete !== -1) {
+      favorites.splice(indexToDelete, 1)
+      favorites = calculateFavoriteId(favorites)
+      await writeFile(new URL('./favorites.json', import.meta.url), JSON.stringify(favorites))
+      res.json(favorites)
+    }
   } catch (error) {
     console.error('Error:', error)
   }
