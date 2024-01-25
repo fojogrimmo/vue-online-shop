a1eee64a9ec8916035f3d6719fb21c2e1b9eae0c
fojogrimmo <afanasyvekarindm@gmail.com>
2024-01-25 23:16:00 +0300

    Install & use root Vue

--- src/main.js
+++ src/main.js
@@ -1,5 +1,23 @@
-import { createApp } from 'vue'
 import './assets/main.css'
+
+import { createApp } from 'vue'
+import { createRouter, createWebHistory } from 'vue-router'
 import App from './App.vue'
 
-createApp(App).mount('#app')
+import Home from './pages/Home.vue'
+import Favorites from './pages/Favorites.vue'
+
+const app = createApp(App)
+
+const routes = [
+  { path: '/', name: 'Home', component: Home },
+  { path: '/favorites', name: 'Favorites', component: Favorites }
+]
+
+const router = createRouter({
+  history: createWebHistory(),
+  routes
+})
+
+app.use(router)
+app.mount('#app')
