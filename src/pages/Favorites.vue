f4a0078213233b18f667eae6979433b318a98d20
fojogrimmo <afanasyvekarindm@gmail.com>
2024-01-26 22:29:55 +0300

    Add Favorites page

--- src/pages/Favorites.vue
+++ src/pages/Favorites.vue
@@ -1,3 +1,66 @@
+<script setup>
+import { onMounted, ref } from 'vue'
+import axios from 'axios'
+
+import CardList from '../components/CardList.vue'
+const favorites = ref([])
+
+const addToFavorite = async (item) => {
+  try {
+    if (!item.isFavorite) {
+      const obj = {
+        item_id: item.id
+      }
+      item.isFavorite = true
+      const { data } = await axios.post('http://localhost:3000/api/favorites/', obj)
+      favorites.value.push(data)
+      item.favoriteId = data.favorite_id
+    } else {
+      console.log('item is already in favorites')
+    }
+  } catch (error) {
+    console.error('Error:', error)
+  }
+}
+
+// const onClickAddCart = async (item) => {
+//   try {
+//     if (!item.isAdded) {
+//       // логика добавления в корзину
+//     } else {
+//     }
+//   } catch (error) {
+//     console.error('Error:', error)
+//   }
+// }
+
+// const deleteFavoriteItem = async (item) => {
+//   try {
+//     item.isFavorite = false
+//     await axios.delete(`http://localhost:3000/api/favorites/${item.favoriteId}`)
+//     item.favoriteId = null
+//   } catch (error) {
+//     console.error('Error:', error)
+//   }
+// }
+
+onMounted(async () => {
+  try {
+    const { data } = await axios.get('http://localhost:3000/api/favorites/')
+    favorites.value = data.map((item) => ({
+      ...item,
+      isFavorite: true
+    }))
+    console.log(data)
+  } catch (error) {
+    console.log('Error:', error)
+  }
+})
+</script>
+
 <template>
-  <h1>Wishlist</h1>
+  <div class="h-screen">
+    <h1 class="text-3xl font-bold mb-10">Wishlist</h1>
+    <CardList :items="favorites" @add-to-favorite="addToFavorite" @add-to-cart="onClickAddCart" />
+  </div>
 </template>
