abc5d77ba38a982c0e98a06afabbdae8a87ea347
fojogrimmo <afanasyvekarindm@gmail.com>
2024-07-11 14:35:14 +0300

    Trasfering data from JSON to PostgeSQL + add db creation script

--- src/api/server.js
+++ src/api/server.js
@@ -1,12 +1,12 @@
 import express from 'express'
 import cors from 'cors'
-import pool from './db.js';
-import bcrypt from 'bcrypt';
-import jwt from 'jsonwebtoken';
+import pool from '../database/db.js/index.js'
+import bcrypt from 'bcrypt'
+import jwt from 'jsonwebtoken'
 
 const app = express()
 const port = 3000
-const secretKey = 'very_secret_key';
+const secretKey = 'very_secret_key'
 
 app.use(express.json())
 app.use((req, res, next) => {
@@ -26,240 +26,241 @@ app.get('/', (req, res) => {
 })
 
 app.post('/api/signup', async (req, res) => {
-  const { name, email, password, is_admin } = req.body;
-  
+  const { name, email, password, is_admin } = req.body
+
   try {
-    const hashedPassword = await bcrypt.hash(password, 10);
-    
+    const hashedPassword = await bcrypt.hash(password, 10)
+
     const result = await pool.query(
       'INSERT INTO users (name, email, password, is_admin) VALUES ($1, $2, $3, $4) RETURNING *',
       [name, email, hashedPassword, is_admin]
-    );
+    )
 
-    res.status(201).json(result.rows[0]);
+    res.status(201).json(result.rows[0])
   } catch (error) {
-    console.error('Error:', error);
-    res.status(500).send('Internal Server Error');
+    console.error('Error:', error)
+    res.status(500).send('Internal Server Error')
   }
-});
+})
 
 app.post('/api/login', async (req, res) => {
-  const { email, password } = req.body;
+  const { email, password } = req.body
 
   if (!email || !password) {
-    return res.status(400).json({ message: 'All fields are required' });
+    return res.status(400).json({ message: 'All fields are required' })
   }
 
   try {
-    const result = await pool.query('SELECT * FROM users WHERE email = $1', [email]);
-    const user = result.rows[0];
+    const result = await pool.query('SELECT * FROM users WHERE email = $1', [email])
+    const user = result.rows[0]
 
     if (!user) {
-      return res.status(401).json({ message: 'Invalid email or password' });
+      return res.status(401).json({ message: 'Invalid email or password' })
     }
 
-    let isValidPassword;
+    let isValidPassword
     if (user.is_admin) {
-      isValidPassword = (password === user.password);
+      isValidPassword = password === user.password
     } else {
-      isValidPassword = await bcrypt.compare(password, user.password);
+      isValidPassword = await bcrypt.compare(password, user.password)
     }
 
-   
     if (!isValidPassword) {
-      return res.status(401).json({ message: 'Invalid email or password' });
+      return res.status(401).json({ message: 'Invalid email or password' })
     }
 
-    const token = jwt.sign({ userId: user.id, email: user.email }, secretKey, { expiresIn: '1h' });
+    const token = jwt.sign({ userId: user.id, email: user.email }, secretKey, { expiresIn: '1h' })
 
-    res.json({ token, user: { id: user.id, email: user.email, name: user.name , is_admin: user.is_admin }});
+    res.json({
+      token,
+      user: { id: user.id, email: user.email, name: user.name, is_admin: user.is_admin }
+    })
   } catch (error) {
-    console.error('Error:', error);
-    res.status(500).send('Internal Server Error');
+    console.error('Error:', error)
+    res.status(500).send('Internal Server Error')
   }
-});
+})
 
 app.get('/api/users', async (req, res) => {
   try {
-    const result = await pool.query('SELECT * FROM users');
-    res.json(result.rows);
+    const result = await pool.query('SELECT * FROM users')
+    res.json(result.rows)
   } catch (error) {
-    console.error('Error:', error);
-    res.status(500).send('Internal Server Error');
+    console.error('Error:', error)
+    res.status(500).send('Internal Server Error')
   }
-});
+})
 
 app.post('/api/users', async (req, res) => {
-  const { name, email, password, is_admin } = req.body;
+  const { name, email, password, is_admin } = req.body
 
   if (!name || !email || !password) {
-    return res.status(400).json({ message: 'All fields are required' });
+    return res.status(400).json({ message: 'All fields are required' })
   }
 
   try {
-    const hashedPassword = await bcrypt.hash(password, 10);
+    const hashedPassword = await bcrypt.hash(password, 10)
     const result = await pool.query(
       'INSERT INTO users (name, email, password, is_admin) VALUES ($1, $2, $3, $4) RETURNING *',
       [name, email, hashedPassword, is_admin]
-    );
+    )
 
-    res.status(201).json(result.rows[0]);
+    res.status(201).json(result.rows[0])
   } catch (error) {
-    console.error('Error:', error);
-    res.status(500).send('Internal Server Error');
+    console.error('Error:', error)
+    res.status(500).send('Internal Server Error')
   }
-});
-
+})
 
 app.patch('/api/users/:id/admin', async (req, res) => {
-  const userId = req.params.id;
-  const { is_admin } = req.body;
+  const userId = req.params.id
+  const { is_admin } = req.body
 
   try {
-    await pool.query('UPDATE users SET is_admin = $1 WHERE id = $2', [is_admin, userId]);
-    res.status(200).send('Admin status updated');
+    await pool.query('UPDATE users SET is_admin = $1 WHERE id = $2', [is_admin, userId])
+    res.status(200).send('Admin status updated')
   } catch (error) {
-    console.error('Error:', error);
-    res.status(500).send('Internal Server Error');
+    console.error('Error:', error)
+    res.status(500).send('Internal Server Error')
   }
-});
+})
 
 app.delete('/api/users/:id', async (req, res) => {
-  const userId = req.params.id;
+  const userId = req.params.id
 
   try {
-    const result = await pool.query('DELETE FROM users WHERE id = $1', [userId]);
+    const result = await pool.query('DELETE FROM users WHERE id = $1', [userId])
 
     if (result.rowCount === 0) {
-      return res.status(404).json({ message: 'User not found' });
+      return res.status(404).json({ message: 'User not found' })
     }
 
-    res.status(200).json({ message: 'User deleted successfully' });
+    res.status(200).json({ message: 'User deleted successfully' })
   } catch (error) {
-    console.error('Error:', error);
-    res.status(500).send('Internal Server Error');
+    console.error('Error:', error)
+    res.status(500).send('Internal Server Error')
   }
-});
+})
 
 app.get('/api/products', async (req, res) => {
   try {
-    const result = await pool.query('SELECT * FROM products');
-    let products = result.rows;
+    const result = await pool.query('SELECT * FROM products')
+    let products = result.rows
 
-    products = products.map(product => ({
+    products = products.map((product) => ({
       ...product,
       price: parseFloat(product.price)
-    }));
+    }))
 
-    const sortOption = req.query.sort;
-    const searchQuery = req.query.title ? req.query.title.toLowerCase() : '';
+    const sortOption = req.query.sort
+    const searchQuery = req.query.title ? req.query.title.toLowerCase() : ''
 
     if (sortOption === 'priceHighToLow') {
-      products.sort((a, b) => b.price - a.price);
+      products.sort((a, b) => b.price - a.price)
     } else if (sortOption === 'priceLowToHigh') {
-      products.sort((a, b) => a.price - b.price);
+      products.sort((a, b) => a.price - b.price)
     }
 
     const filteredProducts = products.filter(
       (product) =>
         product.title.toLowerCase().includes(searchQuery) ||
         product.subtitle.toLowerCase().includes(searchQuery)
-    );
+    )
 
-    res.json(filteredProducts);
+    res.json(filteredProducts)
   } catch (error) {
-    console.error('Error:', error);
-    res.status(500).send('Internal Server Error');
+    console.error('Error:', error)
+    res.status(500).send('Internal Server Error')
   }
 })
 
 app.post('/api/products', async (req, res) => {
-  const { title, subtitle, price, imageurl } = req.body;
+  const { title, subtitle, price, imageurl } = req.body
 
   if (!title || !subtitle || !price || !imageurl) {
-    return res.status(400).send('All fields are required');
+    return res.status(400).send('All fields are required')
   }
 
   try {
     const result = await pool.query(
       'INSERT INTO products (title, subtitle, price, imageurl) VALUES ($1, $2, $3, $4) RETURNING *',
       [title, subtitle, price, imageurl]
-    );
-    
+    )
 
-    const newProduct = result.rows[0];
-    res.status(201).json(newProduct);
+    const newProduct = result.rows[0]
+    res.status(201).json(newProduct)
   } catch (error) {
-    console.error('Error:', error);
-    res.status(500).send('Internal Server Error');
+    console.error('Error:', error)
+    res.status(500).send('Internal Server Error')
   }
-});
+})
 
 app.delete('/api/products/:id', async (req, res) => {
-  const productId = req.params.id;
+  const productId = req.params.id
 
   try {
-    const result = await pool.query('DELETE FROM products WHERE id = $1 RETURNING *', [productId]);
+    const result = await pool.query('DELETE FROM products WHERE id = $1 RETURNING *', [productId])
 
     if (result.rowCount === 0) {
-      return res.status(404).send('Product not found');
+      return res.status(404).send('Product not found')
     }
 
-    res.status(200).json(result.rows[0]);
+    res.status(200).json(result.rows[0])
   } catch (error) {
-    console.error('Error:', error);
-    res.status(500).send('Internal Server Error');
+    console.error('Error:', error)
+    res.status(500).send('Internal Server Error')
   }
-});
+})
 
 app.get('/api/favorites', async (req, res) => {
   try {
-    const result = await pool.query('SELECT * FROM favorites');
-    res.json(result.rows);
+    const result = await pool.query('SELECT * FROM favorites')
+    res.json(result.rows)
   } catch (error) {
-    console.error('Error:', error);
-    res.status(500).send('Internal Server Error');
+    console.error('Error:', error)
+    res.status(500).send('Internal Server Error')
   }
 })
 
 app.post('/api/favorites', async (req, res) => {
   try {
-    const favorite = req.body;
+    const favorite = req.body
 
-    const productResult = await pool.query('SELECT * FROM products WHERE id = $1', [favorite.item_id]);
-    const product = productResult.rows[0];
+    const productResult = await pool.query('SELECT * FROM products WHERE id = $1', [
+      favorite.item_id
+    ])
+    const product = productResult.rows[0]
 
     if (product) {
       const result = await pool.query(
         'INSERT INTO favorites (item_id, title, subtitle, price, imageurl) VALUES ($1, $2, $3, $4, $5) RETURNING *',
         [favorite.item_id, product.title, product.subtitle, product.price, product.imageurl]
-      );
-      res.json(result.rows[0]);
+      )
+      res.json(result.rows[0])
     } else {
-      res.status(404).send('Product not found');
+      res.status(404).send('Product not found')
     }
   } catch (error) {
-    console.error('Error:', error);
-    res.status(500).send('Internal Server Error');
+    console.error('Error:', error)
+    res.status(500).send('Internal Server Error')
   }
 })
 
 app.delete('/api/favorites/:favorite_id', async (req, res) => {
   try {
-    const favoriteId = parseInt(req.params.favorite_id, 10);
-    await pool.query('DELETE FROM favorites WHERE favorite_id = $1', [favoriteId]);
-    res.status(204).send();
+    const favoriteId = parseInt(req.params.favorite_id, 10)
+    await pool.query('DELETE FROM favorites WHERE favorite_id = $1', [favoriteId])
+    res.status(204).send()
   } catch (error) {
-    console.error('Error:', error);
-    res.status(500).send('Internal Server Error');
+    console.error('Error:', error)
+    res.status(500).send('Internal Server Error')
   }
 })
 
 app.get('/api/orders', async (req, res) => {
   try {
-    const result = await pool.query('SELECT * FROM orders');
-    res.json(result.rows);
-
+    const result = await pool.query('SELECT * FROM orders')
+    res.json(result.rows)
   } catch (error) {
     console.log('Error:', error)
   }
@@ -267,16 +268,22 @@ app.get('/api/orders', async (req, res) => {
 
 app.post('/api/orders', async (req, res) => {
   try {
-    const order = req.body;
+    const order = req.body
 
-    const orderResult = await pool.query('INSERT INTO orders (total_price) VALUES ($1) RETURNING id', [order.totalPrice]);
-    const orderId = orderResult.rows[0].id;
+    const orderResult = await pool.query(
+      'INSERT INTO orders (total_price) VALUES ($1) RETURNING id',
+      [order.totalPrice]
+    )
+    const orderId = orderResult.rows[0].id
 
     for (const item of order.items) {
-      await pool.query('INSERT INTO order_items (order_id, product_id) VALUES ($1, $2)', [orderId, item.id]);
+      await pool.query('INSERT INTO order_items (order_id, product_id) VALUES ($1, $2)', [
+        orderId,
+        item.id
+      ])
     }
 
-    res.status(201).json({ message: 'Order created successfully' });
+    res.status(201).json({ message: 'Order created successfully' })
   } catch (error) {
     console.error('Error:', error)
   }
